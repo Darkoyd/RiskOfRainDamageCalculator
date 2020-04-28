f9225f5f191fabe01749e5502e3a4a2f01ba49f2
Nicolás Londoño <nilocuru@hotmail.com>
2020-04-28 16:50:45 -0500

    Intial commit

---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <b-container>
+    <b-row
+      v-for="row in Math.ceil(survivors.length/5)"
+      :key="row.index"
+      align-h="center"
+      class="row"
+    >
+      <b-col
+        v-for="survivor in survivors.slice((row-1)*5 , row*5)"
+        :key="survivor.index"
+        align="center"
+      >
+        {{ survivor.name }}
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      survivors: [
+        {
+          name: 'Acrid'
+        },
+        {
+          name: 'Artificer'
+        },
+        {
+          name: 'Commando'
+        },
+        {
+          name: 'Engineer'
+        },
+        {
+          name: 'Huntress'
+        },
+        {
+          name: 'Loader'
+        },
+        {
+          name: 'Mercenary'
+        },
+        {
+          name: 'MULL-T'
+        },
+        {
+          name: 'REX'
+        }
+
+      ]
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style>
+
+</style>
